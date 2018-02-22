facedb8fb5ac862528b7e75bbb749492ad24a2c0
aistreb <anna.shkulepova@gmail.com>
2018-02-22 20:40:51 +0200

    LinkedinLoginTest is updated

--- src/test/java/LinkedinLoginTest.java
+++ src/test/java/LinkedinLoginTest.java
@@ -3,35 +3,63 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Assert;
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
 
 public class LinkedinLoginTest {
+    WebDriver driver;
+
+    @BeforeClass
+    public void beforeClass () {
+
+    }
+
+    @AfterClass
+    public void afterClass () {
+
+    }
+
+    @BeforeMethod
+    public void beforeTest () {
+        driver = new FirefoxDriver();
+        driver.get("https://www.linkedin.com/");
+    }
+
+    @AfterMethod
+    public void afterTest () {
+        driver.close();
+    }
 
     @Test
-    public void successfullLoginTest() {
-        WebDriver driver = new FirefoxDriver();
-        driver.get("https://www.linkedin.com/");
+    public void successfullLoginTest() throws InterruptedException {
+
+        String initialPageTitle = driver.getTitle();
+        String initialPageUrl = driver.getCurrentUrl();
+
+        Assert.assertEquals(driver.getTitle(), "LinkedIn: Log In or Sign Up","Login page title is wrong");
         //WebElement emailField = driver.findElement(By.xpath("//*[@id='login-email']"));
         WebElement emailField = driver.findElement (By.id("login-email"));
         WebElement passwordField = driver.findElement (By.id("login-password"));
         WebElement signInButton = driver.findElement (By.id("login-submit"));
 
         emailField.sendKeys("a.iastreb1234@gmail.com");
-        passwordField.sendKeys("150599");
+        passwordField.sendKeys("itea2018");
         signInButton.click();
+        Thread.sleep(5000);
+        WebElement homeIcon = driver.findElement(By.id("feed-tab-icon"));
+        WebElement userIcon = driver.findElement (By.id("profile-nav-item"));
 
-        WebElement alertMessage = driver.findElement(By.id("feed-tab-icon"));
-
-        Assert.assertTrue(alertMessage.isDisplayed(), "No Home icon.");
-
-
+        //Assert.assertTrue(homeIcon.isDisplayed(), "No Home icon.");
+        Assert.assertTrue(userIcon.isDisplayed(), "User icon is not dispayed.");
+       // Assert.assertFalse(driver.getTitle().equals(initialPageTitle), "Page title did not changed after login.");
+        Assert.assertNotEquals(driver.getTitle(), initialPageTitle, "Page title did not changed after login.");
+        Assert.assertNotEquals(driver.getCurrentUrl(), initialPageUrl, "Page url did not changed after login.");
     }
 
 
     @Test
     public void negativeLoginTest() {
-        WebDriver driver = new FirefoxDriver();
-        driver.get("https://www.linkedin.com/");
+
+
         WebElement emailField = driver.findElement (By.id("login-email"));
         WebElement passwordField = driver.findElement (By.id("login-password"));
         WebElement signInButton = driver.findElement (By.id("login-submit"));
